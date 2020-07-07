b11fe5e9164d619a24bce8a37a92d0c4970d2fb4
Tan Theng Lee <thengleetan@gmail.com>
2020-07-07 17:15:04 +0800

    Refactor function statements to arrow functions

--- src/App.js
+++ src/App.js
@@ -19,38 +19,32 @@ const list = [
   }
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
 
-      <hr />
+    <hr />
 
-      <List />
-    </div>
-  );
-}
+    <List />
+  </div>
+);
 
-function List() {
-  return (
-    <div>
-      {list.map(function(item) {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+const List = () => (
+  <div>
+    {list.map(item => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    ))}
+  </div>
+);
 
 export default App;
