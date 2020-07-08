3611bace760fce68183f2a064a407222a6a47a21
Tan Theng Lee <thengleetan@gmail.com>
2020-07-08 14:57:08 +0800

    Destructure props

--- src/App.js
+++ src/App.js
@@ -43,30 +43,24 @@ const App = () => {
   );
 };
 
-const Search = props => (
+const Search = ({ search, onSearch }) => (
   <div>
     <label htmlFor="search">Search: </label>
-    <input
-      id="search"
-      type="text"
-      value={props.search}
-      onChange={props.onSearch}
-    />
+    <input id="search" type="text" value={search} onChange={onSearch} />
   </div>
 );
 
-const List = props => (
+const List = ({ list }) =>
+  list.map(item => <Item key={item.objectID} item={item} />);
+
+const Item = ({ item }) => (
   <div>
-    {props.list.map(item => (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-    ))}
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </div>
 );
 
