c41045eebfb517a86eee6dd7966b899066a6c0b1
Tan Theng Lee <thengleetan@gmail.com>
2020-07-08 15:05:16 +0800

    Advanced props destructuring using spread and rest operators

--- src/App.js
+++ src/App.js
@@ -51,16 +51,16 @@ const Search = ({ search, onSearch }) => (
 );
 
 const List = ({ list }) =>
-  list.map(item => <Item key={item.objectID} item={item} />);
+  list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />);
 
-const Item = ({ item }) => (
+const Item = ({ title, url, author, num_comments, points }) => (
   <div>
     <span>
-      <a href={item.url}>{item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
   </div>
 );
 
