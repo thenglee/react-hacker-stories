f70d37e095a5d15c58e8c19c0a12440596ad73e5
Tan Theng Lee <thengleetan@gmail.com>
2020-07-13 23:00:57 +0800

    Use memoized handler useCallback hook for fetching data

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ const App = () => {
     isError: false
   });
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
@@ -71,6 +71,10 @@ const App = () => {
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = item =>
     dispatchStories({ type: 'REMOVE_STORIES', payload: item });
