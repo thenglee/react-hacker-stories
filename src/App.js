1eafa45236b62d19b532dfffef971cce77fd98a5
Tan Theng Lee <thengleetan@gmail.com>
2020-07-14 11:30:50 +0800

    Refactor to use form for search submit and into standalone form component

--- src/App.js
+++ src/App.js
@@ -46,6 +46,24 @@ const storiesReducer = (state, action) => {
   }
 };
 
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      label="Search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
+
 const App = () => {
   const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -83,25 +101,21 @@ const App = () => {
 
   const handleSearchInput = event => setSearchTerm(event.target.value);
 
-  const handleSearchSubmit = () => setUrl(`${API_ENDPOINT}${searchTerm}`);
+  const handleSearchSubmit = event => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
+  };
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel
-        id="search"
-        label="Search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search</strong>
-      </InputWithLabel>
-
-      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
 
